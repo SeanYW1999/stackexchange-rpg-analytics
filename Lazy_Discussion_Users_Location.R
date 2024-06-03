047f55ee889c3ce63c8d68d00d5a22a5742733a2
Sean Wong <82072708+Lazy-and-leisure1213@users.noreply.github.com>
2024-06-03 17:43:27 +0800

    Add a new column for saving the Location's Nation

--- Lazy_Discussion_Users_Location.R
+++ Lazy_Discussion_Users_Location.R
@@ -17,3 +17,8 @@ User_info <- dbGetQuery(connect,"SELECT Id,
                                         Location 
                                  FROM Users
                                  WHERE Location IS NOT NULL AND Id > 0;")
+
+###add a new column for saving the Location's Nation
+Country <- matrix(NA,nrow(User_info),1)
+User_info <- cbind(User_info,Country)
+User_info[,4] <- User_info[,3]
