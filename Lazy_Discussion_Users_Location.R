f4cba7c931741697433d35e95a739c2bf78601af
Sean Wong <82072708+Lazy-and-leisure1213@users.noreply.github.com>
2024-06-03 18:09:09 +0800

    Statistics on the number of Users in each Country

--- Lazy_Discussion_Users_Location.R
+++ Lazy_Discussion_Users_Location.R
@@ -48,3 +48,13 @@ for (coun in Country_list) {
 User_country <- data.frame(Id = User_country$Id,Displyname = User_country$DisplayName,
                            Country = User_country$Country)
 User_country_new <- User_country[which(User_country$Country!="Europe"),]
+
+### Statistics on the number of Users in each Country
+Country_count <- data.frame()
+for (coun in Country_list_new) {
+  numlist <- list(Country = coun, 
+                  numbers_of_users = nrow(User_country_new[which(User_country_new$Country==coun),]) )
+  Country_count <- rbind(Country_count,numlist)
+}
+Country_count<-Country_count[order(Country_count$numbers_of_users,decreasing = TRUE),]
+row.names(Country_count) <- 1:nrow(Country_count)
